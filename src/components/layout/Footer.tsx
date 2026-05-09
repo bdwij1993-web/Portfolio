62d96425b54c5bed698662de2fa5ef9dd6e066fa
bdwij1993-web <bdwij1993@gmail.com>
2026-05-10 02:59:55 +0530

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -18,7 +18,7 @@ export const Footer = () => {
               <a href="#" className="text-slate-400 hover:text-white transition-colors">
                 <Twitter className="w-5 h-5" />
               </a>
-              <a href="#" className="text-slate-400 hover:text-white transition-colors">
+              <a href="https://www.linkedin.com/in/dwij-bhatt/" className="text-slate-400 hover:text-white transition-colors">
                 <Linkedin className="w-5 h-5" />
               </a>
               
